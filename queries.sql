ca7298db83f1d9a7cafd9874ae3712a6bb746427
Arun Joseph <ajoseps@gmail.com>
2014-01-25 15:12:06 -0800

    added 2/3rds of the queries we need to do

--- queries.sql
+++ queries.sql
@@ -1,11 +1,18 @@
 -- Finds the name of all Actors in the movie Die Another Day --
 SELECT A.first, A.last
 FROM Actor A, MovieActor MA, Movie M
-WHERE M.id = MA.mid AND MA.aid = A.id AND M.title = 'Die Another Day'
+WHERE M.id = MA.mid AND MA.aid = A.id AND M.title = 'Die Another Day';
+
+-- Finds the name of all the movies Pierce Brosnan played in --
+SELECT title
+FROM Actor A, MovieActor MA, Movie M
+WHERE A.first = 'Pierce' AND A.last = 'Brosnan' AND MA.aid = A.id AND M.id = MA.mid;
 
 -- Returns the count of all actors who acted in multiple movies --
-SELECT COUNT(DISTINCT aid)
-FROM MovieActor
-WHERE
-GROUP BY aid
-HAVING COUNT(*)>=2
+-- SELECT COUNT(DISTINCT aid)
+-- FROM MovieActor
+-- WHERE
+-- GROUP BY aid
+-- HAVING COUNT(*)>=2;
+
+
